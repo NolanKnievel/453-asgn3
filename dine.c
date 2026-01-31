194d2a8cb20209bb5b5943ec5f1fe6d1977016ab
Nolan Knievel <nolan.knievel@icloud.com>
2026-01-31 12:05:27 -0800

    add trivial implementation

--- dine.c
+++ dine.c
@@ -7,6 +7,56 @@
 
 
 int main() {
-    printf("hello world!\n");
-    return 1;
+    pid_t ppid;
+    int i;
+
+    int id[NUM_CHILDREN];
+    pthread_t = childid[NUM_CHILDREN];
+
+    // initialize ids array
+    int i;
+    for(i=0; i<NUM_CHILDREN; i++) {
+        id[i] = i;
+    }
+
+    // parent process id
+    ppid = getpid();
+
+    for(i=0; i<NUM_CHILDREN; i++) {
+        int res;
+        res = pthread_create(
+            &childid[i],
+            NULL,
+            child,
+            (void *) &id[i]
+        )
+    }
+
+    // report error if there was one
+    if (-1 == res) {
+        fprintf(stderr, "Child %i: %s\n",i,strerror(res));
+        exit(-1)
+
+    }
+
+    printf("Parent (%d): Hello. \n\n", (int) ppid);
+
+    // wait for ach child to finish
+    for(i=0; i<NUM_CHILDREN; i++) {
+        pthread_join(childid[i], NULL);
+        printf("Parent (%d): child %d exited \n\n", (int ppid, i));
+
+    }
+
+    printf("Parent (%d): Goodbye. \n\n", (int) ppid);
+    return 0;
+
+}
+
+void *child(void *id) {
+    int whoami = *(int *)id;
+    printf("Child %d (%d): Hello.\n\n", whoami, (int) getpid());
+    printf("Child %d (%d): Goodbye.\n\n", whoami, (int) getpid());
+    return NULL;
+
 }
